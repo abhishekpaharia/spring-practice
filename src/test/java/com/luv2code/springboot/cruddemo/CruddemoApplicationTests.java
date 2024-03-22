0efbcc80dca6b28defe2fa247211e6bc491fb27a
abhishekpaharia <abhipaharia12@gmail.com>
2024-03-21 22:46:52 -0400

    intial setup

--- src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gmail.abhipaharia12.restdemo;
+package com.luv2code.springboot.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestdemoApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
