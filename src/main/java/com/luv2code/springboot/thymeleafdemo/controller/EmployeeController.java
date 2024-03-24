8a7da83d372c3d8fa7da7b7ef7f1a1549cc01704
abhishekpaharia <abhipaharia12@gmail.com>
2024-03-24 02:49:09 -0400

    1. completing the save new employee. 2. sorting the employees list using JPARepository QureyMethod.

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -33,6 +35,12 @@ public class EmployeeController {
     public String showFormForAdd(Model theModel){
         Employee theEmployee = new Employee();
         theModel.addAttribute("employee", theEmployee);
-        return "employee/employee-form";
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("employee") Employee theEmployee){
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
     }
 }
