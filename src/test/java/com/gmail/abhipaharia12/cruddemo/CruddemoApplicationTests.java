7d81cd565d83bc1b31a91a61b0af5bbfd0fe713a
abhishekpaharia <abhipaharia12@gmail.com>
2024-03-25 18:13:05 -0400

    inital setup for branch one-2-one-uni-instructor

--- src/test/java/com/gmail/abhipaharia12/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/gmail/abhipaharia12/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gmail.abhipaharia12.restdemo;
+package com.gmail.abhipaharia12.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestdemoApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
